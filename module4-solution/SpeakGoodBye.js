bf379901db8ff183c875dd6affaac84d6b883b06
tushar5544 <84268591+tushar5544@users.noreply.github.com>
2021-05-27 18:07:09 +0530

    Delete module4-solution directory

--- module4-solution/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	var byeSpeaker = {};
-var speakWord = "Good Bye";
-byeSpeaker.speak = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-window.byeSpeaker = byeSpeaker;
-
-})();
--- module4-solution/SpeakHello.js
+++
@@ -1,8 +0,0 @@
-(function(){
- var helloSpeaker = {};
-var speakWord = "Hello";
-helloSpeaker.speak = function (name) {
-  		console.log(speakWord + " " + name);
-	}
-window.helloSpeaker = helloSpeaker;
-})();
